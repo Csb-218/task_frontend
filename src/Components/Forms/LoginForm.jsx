048b73ef9764ac074fac82c7f24464832f5c79be
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-03-26 19:11:18 +0530

    chore

--- src/Components/Forms/LoginForm.jsx
+++ src/Components/Forms/LoginForm.jsx
@@ -42,17 +42,17 @@ export const LoginForm = () => {
 
     <div className="flex flex-wrap w-full">
       <div className="flex flex-col w-full md:w-1/2">
-        <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
+        {/* <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
           <a href="/#" className="p-4 text-xl font-bold text-white bg-black">
             Design.
           </a>
-        </div>
+        </div> */}
         <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
           <p className="text-3xl text-center">
             Welcome.
           </p>
           <form className="flex flex-col pt-3 md:pt-8" onSubmit={formik.handleSubmit}>
-            <div className="flex flex-col pt-4">
+            {/* <div className="flex flex-col pt-4">
               <div className="flex relative ">
                 <span className=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                   <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
@@ -69,8 +69,8 @@ export const LoginForm = () => {
                   className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Email" />
               </div>
-            </div>
-            <div className="flex flex-col pt-4 mb-12">
+            </div> */}
+            {/* <div className="flex flex-col pt-4 mb-12">
               <div className="flex relative ">
                 <span className=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                   <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
@@ -88,9 +88,9 @@ export const LoginForm = () => {
                   placeholder="Password"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className='my-2'>
+            <div className='my-2  flex flex-row justify-center '>
             <GoogleOAuthProvider clientId="370852537833-lghova8etquoqpba6veoafhi49qvujr9.apps.googleusercontent.com">
               <GoogleLogin
                 onSuccess={credentialResponse => {
@@ -104,7 +104,7 @@ export const LoginForm = () => {
                     credential:credential
                   }
                   console.log(user)
-                  
+
                   dispatch(AuthActions.login(user))
 
                   console.log(credentialResponse);
@@ -119,19 +119,19 @@ export const LoginForm = () => {
 
             
 
-            <button type="submit" className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black shadow-md hover:text-black hover:bg-white focus:outline-none focus:ring-2">
+            {/* <button type="submit" className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black shadow-md hover:text-black hover:bg-white focus:outline-none focus:ring-2">
               <span className="w-full">
                 Submit
               </span>
-            </button>
+            </button> */}
           </form>
           <div className="pt-12 pb-12 text-center">
             <p>
-              Don&#x27;t have an account?
+             Please login using Google
 
-              <Link to='/signup' className="font-semibold underline">
+              {/* <Link to='/signup' className="font-semibold underline">
                 Register here.
-              </Link>
+              </Link> */}
 
             </p>
           </div>
